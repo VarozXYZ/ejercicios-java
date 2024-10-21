bab863a739d6318cd7929b223d187602eaba3f78
VarozXYZ <alvaropiro@gmail.com>
2024-10-21 09:19:54 +0200

    cambios en ejercicios

--- Libro.java
+++ Libro.java
@@ -1,39 +1,36 @@
 public class Libro {
     
-    
   String isbn;
   String titulo;
   Integer year;
   Boolean prestado = false;
 
-public Libro(String isbn, String titulo, Integer year) {
-  this.isbn = isbn;
-  this.titulo = titulo;
-  this.year = year;
-  
-}
+  public Libro(String isbn, String titulo, Integer year) {
+    this.isbn = isbn;
+    this.titulo = titulo;
+    this.year = year;
+    }
 
-public void prestar() {
-  this.prestado = true;
-}
-
-public void devolver() {
-  this.prestado = false;
-}
-
-public void estado() {
-  if (prestado) {
-      System.out.println("Libro prestado");
-  } else {
-      System.out.println("Libro no prestado");
+  public void prestar() {
+    this.prestado = true;
   }
-}
 
-public static void main(String[] args) {
-  Libro libro1 = new Libro("9788408297079", "Alas de Onix", 2025);
-  libro1.estado();
-  libro1.prestar();
-  libro1.estado();
-}
+  public void devolver() {
+    this.prestado = false;
+  }
 
+  public void estado() {
+    if (prestado) {
+        System.out.println("Libro prestado");
+    } else {
+        System.out.println("Libro no prestado");
+    }
+  }
+
+  public static void main(String[] args) {
+    Libro libro1 = new Libro("9788408297079", "Alas de Onix", 2025);
+    libro1.estado();
+    libro1.prestar();
+    libro1.estado();
+  }
 }
