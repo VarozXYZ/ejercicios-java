457600a8f144a971c87172a45fdabc64c205edbf
VarozXYZ <alvaropiro@gmail.com>
2024-10-21 09:17:21 +0200

    cambios en ejercicios

--- Caballos.java
+++ Caballos.java
@@ -1,48 +1,48 @@
 public class Caballos {
-  
+
     private String nombre;
     private String color;
     private Integer edad;
     private Integer carrerasGanadas = 0;
     private Integer carrerasPerdidas = 0;
-    private Integer trofeos= 0;
-        
-        
+    private Integer trofeos = 0;
+
     public Caballos(String nombre, String color, Integer edad) {
         this.nombre = nombre;
         this.color = color;
         this.edad = edad;
     }
-    
+
     public int getCarrerasGanadas() {
         return carrerasGanadas;
     }
-    
-    public void setCarrerasGanadas(Integer newCarrerasGanadas){
+
+    public void setCarrerasGanadas(Integer newCarrerasGanadas) {
         this.carrerasGanadas = newCarrerasGanadas;
     }
-    
+
     public int getCarrerasPerdidas() {
         return carrerasPerdidas;
     }
-        
+
     public void setCarrerasPerdidas(Integer newCarrerasPerdidas) {
         this.carrerasPerdidas = newCarrerasPerdidas;
     }
-    
+
     public int getTrofeos() {
         return trofeos;
     }
-    
+
     public void setTrofeos(Integer newTrofeos) {
         this.trofeos = newTrofeos;
     }
-    
+
     public void verDatosCaballo() {
-        System.out.println("El caballo se llama " + this.nombre + ", es de color " + this.color + " y tiene " + this.edad + ". Ha ganado " + this.carrerasGanadas + " y ha perdido " + this.carrerasPerdidas + ". Además tiene " + this.trofeos + " trofeos.");
+        System.out.println("El caballo se llama " + this.nombre + ", es de color " + this.color + " y tiene "
+                + this.edad + ". Ha ganado " + this.carrerasGanadas + " y ha perdido " + this.carrerasPerdidas
+                + ". Además tiene " + this.trofeos + " trofeos.");
     }
 
-    
     public static void main(String[] args) {
         Caballos caballo1 = new Caballos("Gavilan", "blanco", 5);
         caballo1.setCarrerasGanadas(4);
@@ -50,6 +50,5 @@ public class Caballos {
         caballo1.setTrofeos(2);
         caballo1.verDatosCaballo();
     }
-    
-    
+
 }
--- CuentaBanco.java
+++ CuentaBanco.java
@@ -1,25 +1,28 @@
 public class CuentaBanco {
     
-  private Float saldoCuenta;
-  private Float cantidad;
-  private String concepto;
-  private Integer idMovimiento;
-  
-  public CuentaBanco(Float saldoCuenta) {
-      this.saldoCuenta = saldoCuenta;
+    private Float saldoCuenta;
+    private Float cantidad;
+    private String concepto;
+    private Integer idMovimiento = 0;
+    
+    public CuentaBanco(Float saldoCuenta) {
+        this.saldoCuenta = saldoCuenta;
+    }
+    
+    public void MovimientosMes(Float nomina, Float facturaTelefonica, Float facturaLuz) {
+        this.saldoCuenta += nomina;
+        this.saldoCuenta -= facturaTelefonica;
+        this.saldoCuenta -= facturaLuz;
+        this.idMovimiento++;
+        System.out.println("Nuevo saldo: " + this.saldoCuenta);
+    }
+    
+    
+    public static void main(String[] args) {
+        CuentaBanco micuenta = new CuentaBanco(0f);
+        
+        micuenta.MovimientosMes(1200f, 80f, 150f);
+        
+        System.out.println(micuenta.saldoCuenta);
+    }
   }
-  
-  public void MovimientosMes(Float nomina, Float facturaTelefonica, Float facturaLuz) {
-      this.saldoCuenta += nomina;
-      this.saldoCuenta -= facturaTelefonica;
-      this.saldoCuenta -= facturaLuz;
-      System.out.println("Nuevo saldo: " + this.saldoCuenta);
-  }
-  
-  
-  public static void main(String[] args) {
-      CuentaBanco micuenta = new CuentaBanco(1500f);
-      
-      micuenta.MovimientosMes(1200f, 80f, 150f);
-  }
-}
